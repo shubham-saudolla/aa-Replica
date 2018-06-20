23e5ce153389f758f886e5741cc53db6c3c62612
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-21 02:41:32 +0530

    Added the startscreen and some UI

---
+++ aa Replica/Assets/LoadMain.cs
@@ -0,0 +1,20 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMain : MonoBehaviour
+{
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			SceneManager.LoadScene("Main");
+		}
+	}
+}
