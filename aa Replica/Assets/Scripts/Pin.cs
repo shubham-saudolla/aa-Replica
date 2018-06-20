650c2656e108de254a4c22a4120a2a537a233dda
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-20 22:41:10 +0530

    Updated the pin mechanism
    Can spawn multiple pins using the instantiate method and the pinPrefab

--- aa Replica/Assets/Scripts/Pin.cs
+++ aa Replica/Assets/Scripts/Pin.cs
@@ -10,12 +10,12 @@ using UnityEngine;
 public class Pin : MonoBehaviour
 {
 	public float speed = 20f;
-	private bool isPinned = false;
+	private bool _isPinned = false;
 	public Rigidbody2D rb; //drag in the rigidbody in the inspector
 	
 	void Update () 
 	{
-		if(!isPinned)
+		if(!_isPinned)
 			rb.MovePosition(rb.position + Vector2.up * speed* Time.deltaTime); //using rb.position as iit returns a Vector2. Transform.position returns a Vector3
 	}
 
@@ -24,7 +24,7 @@ public class Pin : MonoBehaviour
 		if(col.tag == "Rotator")
 		{
 			transform.SetParent(col.transform); //parenting the pin to the rotator
-			isPinned = true;
+			_isPinned = true;
 		}
 	}
 }
---
+++ aa Replica/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject pinPrefab;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0)) //spacebar or left mouse click
+		{
+			SpawnPin();
+		}
+	}
+
+	void SpawnPin()
+	{
+		Instantiate(pinPrefab, transform.position, transform.rotation); //spawn at the PinSpawnPoint with it's position and rotation
+	}
+}
