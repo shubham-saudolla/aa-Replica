e2196f5015f0ff29c2d09833a53af12793e449a7
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-20 22:20:40 +0530

    Added pin movement

---
+++ aa Replica/Assets/Scripts/Pin.cs
@@ -0,0 +1,25 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+	public float speed = 20f;
+	public Rigidbody2D rb; //drag in the rigidbody in the inspector
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update () 
+	{
+		rb.MovePosition(rb.position + Vector2.up * speed* Time.deltaTime); //using rb.position as iit returns a Vector2. Transform.position returns a Vector3
+
+	}
+}
